a2e2ba21104ee43deb85092d069efc853aab5dd1
Ron Kurr <kurr@kurron.org>
2016-08-18 16:55:26 -0400

    Use Compose to deploy hello world containers

--- scripts/02-deploy-hello-world.sh
+++ scripts/02-deploy-hello-world.sh
@@ -1,33 +1,16 @@
 #!/bin/bash
 
-ONE="docker run --detach \
-                --name=hello-one \
-                --net=none \
-                dockercloud/hello-world:latest"
-
-TWO="docker run --detach \
-                --name=hello-two \
-                --net=none \
-                dockercloud/hello-world:latest"
-
-THREE="docker run --detach \
-                  --name=hello-three \
-                  --net=none \
-                  dockercloud/hello-world:latest"
-
 # point to the swarm manager
 eval "$(docker-machine env --swarm bravo)"
 
 docker info
 
-echo Deploying the first container
-$ONE
+echo Creating overlay network for the application containers
+docker network create --driver overlay showcase
+docker network ls
 
-echo Deploying the second container
-$TWO
-
-echo Deploying the third container
-$THREE
+echo Deploy containers via Docker Compose
+docker-compose --file compose/hello-world.yml up -d
 
 echo Displaying status of the containers in the cluster
 docker ps
