3d366adae15a2ac24fc4894ce9db008d395d0b97
Ron Kurr <kurr@kurron.org>
2016-08-18 16:57:12 -0400

    Use Compose to clean up hello world containers

--- scripts/03-deploy-application.sh
+++ scripts/03-deploy-application.sh
@@ -1,24 +1,11 @@
 #!/bin/bash
 
-ONE="docker rm --force hello-one"
-TWO="docker rm --force hello-two"
-THREE="docker rm --force hello-three"
-
 # point to the swarm manager
 eval "$(docker-machine env --swarm bravo)"
 
-echo Removing the first container
-$ONE
-
-echo Removing the second container
-$TWO
-
-echo Removing the third container
-$THREE
+echo Removing hello world containers
+docker-compose --file compose/hello-world.yml down --remove-orphans
 
 echo Displaying status of the containers in the cluster
 docker ps
 
-echo Creating overlay network for the application containers
-docker network create --driver overlay --subnet 192.168.50.50/24 showcase
-docker network ls
