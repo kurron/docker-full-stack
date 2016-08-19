ff08375c27bb69d515bdbdde7999bc50b64a1575
Ron Kurr <kurr@kurron.org>
2016-08-19 09:40:11 -0400

    Show how to scale containers via Compose

--- scripts/02-deploy-hello-world.sh
+++ scripts/02-deploy-hello-world.sh
@@ -6,11 +6,15 @@ eval "$(docker-machine env --swarm bravo)"
 docker info
 
 echo Creating overlay network for the application containers
-docker network create --driver overlay showcase
+ONE="docker network create --driver overlay showcase"
+echo $ONE
+$TWO
 docker network ls
 
 echo Deploy containers via Docker Compose
-docker-compose --file compose/hello-world.yml up -d
+TWO="docker-compose --file compose/hello-world.yml up -d"
+echo $TWO
+$TWO
 
 echo Displaying status of the containers in the cluster
 docker ps
