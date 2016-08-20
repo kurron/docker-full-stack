4bc0ddfa31499ed4fbbfdb0db9092da6cfa1c5fb
Ron Kurr <kurr@kurron.org>
2016-08-20 13:46:11 -0400

    Install Busybox for DNS experimentation

---
+++ scripts/14-deploy-busybox.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo Install Busybox
+docker-machine ssh bravo sudo docker service create --mode replicated \
+                                                    --replicas 1 \
+                                                    --name busybox \
+                                                    --update-delay 10s \
+                                                    --network showcase \
+                                                    busybox:latest
+
+echo Inspect the service
+docker-machine ssh bravo sudo docker service inspect --pretty busybox
+
+echo List the logical status of the replicated service
+docker-machine ssh bravo sudo docker service ls
+
+echo List where the service is running
+watch 'docker-machine ssh bravo sudo docker service ps busybox'
+
