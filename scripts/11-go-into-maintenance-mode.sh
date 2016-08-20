c35814cba6d1eea78933b12fbe5b31e4b4f5f7e3
Ron Kurr <kurr@kurron.org>
2016-08-20 13:26:04 -0400

    Do a better job showing the status when in maitenance mode

--- scripts/11-go-into-maintenance-mode.sh
+++ scripts/11-go-into-maintenance-mode.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 echo Put echo into maintenance mode 
-docker-machine ssh bravo sudo docker node update --availability drain echo 
+docker-machine ssh bravo sudo docker node update --availability drain echo
 
 echo Inspect the node
-docker-machine ssh bravo sudo docker node inspect --pretty echo 
+docker-machine ssh bravo sudo docker node inspect --pretty echo
+
+echo Inspect the status of the Swarm
+docker-machine ssh bravo sudo docker node ls
 
 echo Monitor the transition between versions
 watch 'docker-machine ssh bravo sudo docker service ps redis'
--- scripts/12-go-into-active-mode.sh
+++ scripts/12-go-into-active-mode.sh
@@ -6,3 +6,6 @@ docker-machine ssh bravo sudo docker node update --availability active echo
 echo Inspect the node
 docker-machine ssh bravo sudo docker node inspect --pretty echo 
 
+echo Inspect the status of the Swarm
+docker-machine ssh bravo sudo docker node ls
+
