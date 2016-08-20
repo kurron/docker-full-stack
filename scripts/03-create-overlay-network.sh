be6c7d553b99e504e6405682badf403e74930130
Ron Kurr <kurr@kurron.org>
2016-08-20 11:25:26 -0400

    Moved network creation to its own script

---
+++ scripts/03-create-overlay-network.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo Creating overlay network for the application containers
+docker-machine ssh bravo sudo docker network create --driver overlay showcase 
+docker-machine ssh bravo sudo docker network ls 
+
--- scripts/04-deploy-hello-world.sh
+++ scripts/04-deploy-hello-world.sh
@@ -5,12 +5,6 @@ eval "$(docker-machine env --swarm bravo)"
 
 docker info
 
-echo Creating overlay network for the application containers
-ONE="docker network create --driver overlay showcase"
-echo $ONE
-$TWO
-docker network ls
-
 echo Deploy containers via Docker Compose
 TWO="docker-compose --file compose/hello-world.yml up -d"
 echo $TWO
