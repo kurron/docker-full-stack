c667a2f35b3560b0fc0278e8321d6b4602b7bc3a
Ron Kurr <kurr@kurron.org>
2016-08-20 13:31:29 -0400

    Do a better job showing the status of the network

--- scripts/03-create-overlay-network.sh
+++ scripts/03-create-overlay-network.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo Creating overlay network for the application containers
-docker-machine ssh bravo sudo docker network create --driver overlay showcase 
-docker-machine ssh bravo sudo docker network ls 
+docker-machine ssh bravo sudo docker network create --driver overlay showcase
+docker-machine ssh bravo sudo docker network ls
+docker-machine ssh bravo sudo docker network inspect showcase
 
