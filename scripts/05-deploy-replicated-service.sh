6c9fe171d40583811a5faef0ecfbfcb95a57acf6
Ron Kurr <kurr@kurron.org>
2016-08-20 12:22:23 -0400

    Found a better way to list the status of services

--- scripts/05-deploy-replicated-service.sh
+++ scripts/05-deploy-replicated-service.sh
@@ -3,13 +3,12 @@
 echo Deploying a replicated service
 docker-machine ssh bravo sudo docker service create --mode replicated --replicas=2 --name hello-replicated alpine ping docker.com
 
+echo Inspect the service
+docker-machine ssh bravo sudo docker service inspect --pretty hello-replicated
+
 echo List the logical status of the replicated service
 docker-machine ssh bravo sudo docker service ls
 
-echo List the physical status of the replicated service
-docker-machine ssh bravo sudo docker node ps bravo
-docker-machine ssh bravo sudo docker node ps charlie
-docker-machine ssh bravo sudo docker node ps delta
-docker-machine ssh bravo sudo docker node ps echo
-
+echo List where the service is running
+docker-machine ssh bravo sudo docker service ps hello-replicated
 
