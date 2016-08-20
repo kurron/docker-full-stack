cd3465baf23e2f056e27cd446e5fd878e8c1b6f9
Ron Kurr <kurr@kurron.org>
2016-08-20 12:24:31 -0400

    Found a better way to list the status of services

--- scripts/06-deploy-constrained-service.sh
+++ scripts/06-deploy-constrained-service.sh
@@ -7,13 +7,12 @@ docker-machine ssh bravo sudo docker service create --mode replicated \
                                                     --constraint 'node.role==worker' \
                                                     alpine ping docker.com
 
+echo Inspect the service
+docker-machine ssh bravo sudo docker service inspect --pretty hello-constrained
+
 echo List the logical status of the constrained service
 docker-machine ssh bravo sudo docker service ls
 
-echo List the physical status of the constrained service
-docker-machine ssh bravo sudo docker node ps bravo
-docker-machine ssh bravo sudo docker node ps charlie
-docker-machine ssh bravo sudo docker node ps delta
-docker-machine ssh bravo sudo docker node ps echo
-
+echo List where the service is running
+docker-machine ssh bravo sudo docker service ps hello-constrained
 
