37996de0244732432040839d94084d8ab5cc7b87
Ron Kurr <kurr@kurron.org>
2016-08-20 13:37:23 -0400

    Deploy Nginx

---
+++ scripts/13-deploy-nginx.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo Install Nginx 
+docker-machine ssh bravo sudo docker service create --mode replicated \
+                                                    --replicas 3 \
+                                                    --name nginx \
+                                                    --update-delay 10s \
+                                                    --network showcase \
+                                                    nginx:latest
+
+echo Inspect the service
+docker-machine ssh bravo sudo docker service inspect --pretty nginx
+
+echo List the logical status of the replicated service
+docker-machine ssh bravo sudo docker service ls
+
+echo List where the service is running
+watch 'docker-machine ssh bravo sudo docker service ps nginx'
+
