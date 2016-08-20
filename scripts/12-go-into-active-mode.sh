1a50dda2d0eb40fd42b76c76a166e26d399d8dd9
Ron Kurr <kurr@kurron.org>
2016-08-20 13:11:41 -0400

    can put a node into active mode

---
+++ scripts/12-go-into-active-mode.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo Put echo into active mode 
+docker-machine ssh bravo sudo docker node update --availability active echo 
+
+echo Inspect the node
+docker-machine ssh bravo sudo docker node inspect --pretty echo 
+
