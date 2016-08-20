db5b0ef661525763af9e12077178e006d71c1cef
Ron Kurr <kurr@kurron.org>
2016-08-20 13:09:04 -0400

    can put a node into maintenance mode

---
+++ scripts/11-go-into-maintenance-mode.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo Put echo into maintenance mode 
+docker-machine ssh bravo sudo docker node update --availability drain echo 
+
+echo Inspect the node
+docker-machine ssh bravo sudo docker node inspect --pretty echo 
+
+echo Monitor the transition between versions
+watch 'docker-machine ssh bravo sudo docker service ps redis'
+
